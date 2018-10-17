532527aceb6daa5a4c8dede0d1bcb8f84398e295
Rui Guedes <ruiguedes12@hotmail.com>
2018-10-17 09:15:25 +0100

    Updated board size. Code formatting

--- source/listfunc.pl
+++ source/listfunc.pl
@@ -1,37 +1,49 @@
-board([ ['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_'],
-		['_', '_', '_', '_', '_', '_', '_', '_', '_']
+board([ ['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
+		['_', '_', '_', '_', '_', '_', '_', '_', '_','_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
 	  ]).
 
-getPiece(LinNo, ColNo, Board, Piece) :- getLine(LinNo, Board, Line), getColumn(Line, ColNo, Piece).
+getPiece(LinNo, ColNo, Board, Piece):-	getLine(LinNo, Board, Line), 
+									   	getColumn(Line, ColNo, Piece).
 
 getLine(1, [Line|_], Line).
-getLine(LinNo, [_|RestBoard], Line) :- 	LinNo > 1, 
-											PrevLineNo is LinNo - 1, 
-											getLine(PrevLineNo, RestBoard, Line).
+getLine(LinNo, [_|RestBoard], Line):- 	LinNo > 1, 
+									  	PrevLineNo is LinNo - 1, 
+									  	getLine(PrevLineNo, RestBoard, Line).
 
 getColumn([Piece|_], 1, Piece).
-getColumn([_|RestLine], ColNo, Piece) :- 	ColNo > 1, 
-											PrevColNo is ColNo - 1, 
-											getColumn(RestLine, PrevColNo, Piece). 
+getColumn([_|RestLine], ColNo, Piece):-	ColNo > 1, 
+										PrevColNo is ColNo - 1, 
+										getColumn(RestLine, PrevColNo, Piece). 
 											
 
-setPiece(LinNo, ColNo, OldBoard, NewBoard, Piece) :- 	getLine(LinNo, OldBoard, OldLine), 
-														setColumn(ColNo, Piece, OldLine, NewLine),
-														setLine(LinNo, OldBoard, NewBoard, NewLine).
+setPiece(LinNo, ColNo, OldBoard, NewBoard, Piece):-	getLine(LinNo, OldBoard, OldLine), 
+													setColumn(ColNo, Piece, OldLine, NewLine),
+													setLine(LinNo, OldBoard, NewBoard, NewLine).
 
 setColumn(1, Piece, [_|T], [Piece|T]).
-setColumn(ColNo, Piece, [H|T], [H|R]) :- 	ColNo > 1, 
+setColumn(ColNo, Piece, [H|T], [H|R]) :-	ColNo > 1, 
 											PrevColNo is ColNo - 1,
 											setColumn(PrevColNo, Piece, T, R).
 							
 setLine(1, [_|T], [NewLine|T], NewLine).					
-setLine(LinNo, [H|T], [H|R], NewLine) :- 	LinNo > 1,
+setLine(LinNo, [H|T], [H|R], NewLine) :-	LinNo > 1,
 											PrevLinNo is LinNo - 1,
 											setLine(PrevLinNo, T, R, NewLine).
