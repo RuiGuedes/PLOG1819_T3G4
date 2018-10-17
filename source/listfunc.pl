06b4260d071ff39b62aeeba561468beafcf8995d
Rui Guedes <ruiguedes12@hotmail.com>
2018-10-17 16:05:36 +0100

    Board display complete. Also fixed getLine and setLine funtions

--- source/listfunc.pl
+++ source/listfunc.pl
@@ -5,7 +5,7 @@ board([ ['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#
 		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-		['#', '#', '#', '#', '#', 'B', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
 		['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
@@ -23,8 +23,8 @@ board([ ['#', '#', '#', '#', '#', '#', '#', '#', '#','#', '#', '#', '#', '#', '#
 getPiece(LinNo, ColNo, Board, Piece):-	getLine(LinNo, Board, Line), 
 									   	getColumn(Line, ColNo, Piece).
 
-getLine(1, [Line|_], Line).
-getLine(LinNo, [_|RestBoard], Line):- 	LinNo > 1, 
+getLine(0, [Line|_], Line).
+getLine(LinNo, [_|RestBoard], Line):- 	LinNo > 0, 
 									  	PrevLineNo is LinNo - 1, 
 									  	getLine(PrevLineNo, RestBoard, Line).
 
@@ -44,17 +44,18 @@ setColumn(ColNo, Piece, [H|T], [H|R]) :-	ColNo > 1,
 											setColumn(PrevColNo, Piece, T, R).
 
 							
-setLine(1, [_|T], [NewLine|T], NewLine).					
-setLine(LinNo, [H|T], [H|R], NewLine) :-	LinNo > 1,
+setLine(0, [_|T], [NewLine|T], NewLine).					
+setLine(LinNo, [H|T], [H|R], NewLine) :-	LinNo > 0,
 											PrevLinNo is LinNo - 1,
 											setLine(PrevLinNo, T, R, NewLine).
 
-displayGame([H]):-displayLine(H), format("A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S", []).
-							
-displayGame([H|T]):- displayLine(H),
-				   	 displaySepLine,
-			    	 displayGame(T).
-				
+displayGame(Board):- format("~n", []), displayGame(Board, 0).
+displayGame([H], LineNumber):-displayLine(H, LineNumber), format("   A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S~n~n", []).												
+displayGame([H|T], LineNumber):-	displayLine(H, LineNumber),
+									displaySepLine,
+									NextLineNumber is LineNumber + 1,
+									displayGame(T, NextLineNumber).	
 
-displaySepLine:- format("|  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |~n", []).
-displayLine(BoardLine):- format("~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p~n", BoardLine).
+displaySepLine:- format("   |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |~n", []).
+displayLine(BoardLine, LineNumber):- LineNumber < 10, format("~p  ", [LineNumber]) ,format("~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p~n", BoardLine).
+displayLine(BoardLine, LineNumber):- LineNumber > 9, format("~p ", [LineNumber]) ,format("~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p--~p~n", BoardLine).
